4390976422ca86fe1efa4d6fa2f538c7f3adc7f3
Sondre Eikanger Kvalø <zapodot@gmail.com>
2012-01-25 22:50:56 +0100

    fix config and example Controller

--- README.markdown
+++ README.markdown
@@ -5,5 +5,30 @@ Constretto Play Module
  * Adds support for using Play's ID mechanism to specify environment. 
  * Makes all the properties in "application.conf" available for injection
  * Injection of configuration for controller classes using Constretto's @Configuration annotation
+ * The module has been developed and tested with Play 1.2.4
 
+Examples:
+--------------------
 
+    public class Application extends Controller {
+    
+    @Configuration(expression = "example.text")
+    static String text;
+
+    @Configuration(expression = "example.number", defaultValue = "0", required = false)
+    static Integer number;
+
+    ...
+    }
+
+If the configuration expression in the first property is not in found in application.conf, Constretto will throw a ConstrettoException (which is a RuntimeException) . 
+
+If you provide a value for the 'defaultValue' property like in the second property that will be used if Constretto can not find a value for the expression in the configuration file.
+
+When you change values for the properties in application.conf and hit reload the Constretto Play modul will automatically re-inject values in to the annotated fields in all your controllers.
+
+Change history
+--------------------
+ * v.01 (built 2012-01-25): basic integration with Play controllers done
+
+If you have experience any issues or have change requests for this module, please use the [GitHub issue tracker](https://github.com/zapodot/constretto-play/issues).
--- constretto-sample/app/controllers/Application.java
+++ constretto-sample/app/controllers/Application.java
@@ -1,9 +1,23 @@
 package controllers;
 
 import org.constretto.annotation.Configuration;
+import org.constretto.annotation.Configure;
 import play.mvc.Controller;
 
 public class Application extends Controller {
+
+    private static class ExampleConfiguration {
+
+        private ExampleConfiguration(String text, Integer number) {
+            this.text = text;
+            this.number = number;
+        }
+
+        private String text;
+
+        @Configuration(expression = "example.number")
+        private Integer number;
+    }
 
     @Configuration(expression = "example.text")
     static String text;
@@ -11,11 +25,28 @@ public class Application extends Controller {
     @Configuration(expression = "example.number")
     static Integer number;
 
+    @Configuration(expression = "example.unknownProperty", defaultValue = "default text", required = false)
+    static String missingValue;
+
+    static ExampleConfiguration configuration;
+
     public static void index() {
         StringBuilder exampleTextBuilder = new StringBuilder("Injected parameters ");
         exampleTextBuilder.append("Text: ").append(text).append('\n');
-        exampleTextBuilder.append("Number: ").append(number);
+        exampleTextBuilder.append("Number: ").append(number).append('\n');
+        exampleTextBuilder.append("missingValue: ").append(missingValue).append('\n');
+        if (configuration != null) {
+            exampleTextBuilder.append("configuration property has been set");
+        }
         renderText(exampleTextBuilder.toString());
+    }
+
+    @Configure
+    public void configurationMethod(
+            @Configuration(expression = "example.text") String text,
+            @Configuration(expression = "example.number") Integer number
+    ) {
+        configuration = new ExampleConfiguration(text, number);
     }
 
 }
